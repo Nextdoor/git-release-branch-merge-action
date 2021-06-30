a37f9a40aa539861817a1b8de63d0738c88abcc4
Matt Wise <matt@nextdoor.com>
2021-06-30 06:24:51 -0700

    Initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,35 +3,65 @@
 # Exit on any failure.
 set -eu
 
-# Figure out our source branch or tag...
-SOURCE_NAME=${GITHUB_REF#refs/*/}
+TAG_NAME=""
 
-if [[ "${GITHUB_REF}" == "refs/pull"* ]]; then
-  echo "Cannot execute merge from a pull request. Exiting."
-  exit 0
-fi
+_set_tag_string() {
+  local CHART_YAML=${INPUT_CHART_DIR}/Chart.yaml
+  local NAME
+  local VERSION
 
-_merge_to_dest_branch() {
+  if [ ! -f "${CHART_YAML}" ]; then
+    echo "::error::Can not find ${CHART_YAML}. Failing."
+    return 1
+  fi
+
+  VERSION=$(yq eval .version "${CHART_YAML}") || return 1
+  NAME=$(yq eval .name "${CHART_YAML}") || return 1
+
+  TAG_NAME="${NAME}-${VERSION}"
+}
+
+_fix_perms() {
   # https://github.com/actions/checkout/issues/164#issuecomment-592281100
   sudo chmod -R ugo+rwX .
-
-  git checkout ${INPUT_RELEASE_BRANCH}
-
-  git log ${INPUT_RELEASE_BRANCH}..${SOURCE_NAME}
-  git merge \
-    --message "[${INPUT_COMMIT_USER_NAME}] Merge from ${SOURCE_NAME}" \
-    --commit \
-    --stat \
-    --no-ff \
-    "${SOURCE_NAME}"
 }
+
+_create_tag() {
+  echo "::debug::Creating tag ${TAG_NAME}..."
+}
+
+_create_release() {
+  [ "${INPUT_CREATE_RELEASE}" == "true" ] || return 0
+
+  echo "::debug::Creating release ${TAG_NAME} from ${TAG_NAME}..."
+
+  if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "::error::Cannot create Github Release without setting GITHUB_TOKEN"
+    return 1
+  fi
+
+  return 0
+  OWNER=
+  REPOSITORY=
+  ACCESS_TOKEN=
+  VERSION=
+  curl --data '{"tag_name": "v$VERSION",
+                "target_commitish": "master",
+                "name": "v$VERSION",
+                "body": "Release of version $VERSION",
+                "draft": false,
+                "prerelease": false}' \
+    https://api.github.com/repos/$OWNER/$REPOSITORY/releases?access_token=$ACCESS_TOKEN
+}
+
 
 # Be really loud and verbose if we're running in VERBOSE mode
 if [ "${INPUT_VERBOSE}" == "true" ]; then
   set -x
   echo "Environment:"
-  env
-  echo "Arguments: $@"
 fi
 
-_merge_to_dest_branch
+_set_tag_string
+_fix_perms
+_create_tag
+_create_release
